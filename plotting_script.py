cb67abb7bf73535c5d21c916471c46c6233d91cf
Braxton Lowers <Braxtonlwrs@outlook.com>
2019-10-08 22:06:41 -0400

    Created final public version of script for analyzing and plotting data from UCF phenol-anisole research

---
+++ plotting_script.py
@@ -0,0 +1,5 @@
+#! python3
+# The purpose of this script is to utilize data from NMRPeaks.csv and analyte_info.json to produce plots of
+# concentration vs corrected chemical shift, perform regression, and output the results in a table
+#
+# author: Braxton Lowers
